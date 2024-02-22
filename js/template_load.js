d0f30cf7500aa7991d2eb849f267d0f06069bec3
Sebidev <sebidev@hotmail.com>
2024-02-22 02:12:16 +0100

    Fix "selected" Menü Sideboard Bug

--- js/template_load.js
+++ js/template_load.js
@@ -1,3 +1,11 @@
+/**
+ * @file template_load.js
+ * @brief Load header and sideboard templates into the current page
+ * 
+ * This file contains the code to load the header and sideboard templates
+ * into the current page. The header and sideboard templates are loaded
+ */
+
 document.addEventListener("DOMContentLoaded", function() {
     fetch('template/header.html')
         .then(response => response.text())
@@ -12,6 +20,19 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.querySelector('#sideboard').innerHTML = data;
+
+            var menuItems = document.querySelectorAll('.sidemenu_element');
+            var currentUrl = new URL(window.location.href.trim().toLowerCase());
+
+            menuItems.forEach(function(menuItem) {
+                var itemUrl = new URL(menuItem.getAttribute('href').trim().toLowerCase(), window.location.origin);
+
+                if (currentUrl.href === itemUrl.href) {
+                    menuItem.classList.add('selected');
+                } else {
+                    menuItem.classList.remove('selected');
+                }
+            });
         })
         .catch(error => {
             console.error('Error:', error);
