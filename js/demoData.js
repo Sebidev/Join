4022e7b9154a7681ac2d2d7fb5868394bf4081ff
Sebidev <sebidev@hotmail.com>
2024-03-06 19:47:47 +0100

    Fix demoData imagePaths

--- js/demoData.js
+++ js/demoData.js
@@ -2,14 +2,14 @@
  * @description Demo Contacts for the user
  */
 let demoContacts = [
-    { id: generateId(), avatarid: rollDice(), name: 'Anton Mayer', email: 'antom@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-0.svg", initials: "AM"},
-    { id: generateId(), avatarid: rollDice(), name: 'Anja Schulz', email: 'schulz@hotmail.com', phone: '+49123456789', imagePath:"http://127.0.0.1:5501/img/Ellipse5-3.svg", initials: "AS"},
-    { id: generateId(), avatarid: rollDice(), name: 'Benedikt Ziegler', email: 'benedikt@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-4.svg", initials: "BZ"},
-    { id: generateId(), avatarid: rollDice(), name: 'David Eisenberg', email: 'davidberg@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-1.svg", initials: "DE",},
-    { id: generateId(), avatarid: rollDice(), name: 'Eva Fischer', email: 'eva@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-2.svg", initials: "EF"},
-    { id: generateId(), avatarid: rollDice(), name: 'Emmanuel Mauer', email: 'emmanuelma@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-1.svg", initials: "EM"},
-    { id: generateId(), avatarid: rollDice(), name: 'Marcel Bauer', email: 'bauer@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-3.svg", initials: "MB"},
-    { id: generateId(), avatarid: rollDice(), name: 'Tatjana Wolf', email: 'wolf@gmail.com', phone: '+49123456789', imagePath: "http://127.0.0.1:5501/img/Ellipse5-2.svg", initials: "TW"},
+    { id: generateId(), avatarid: rollDice(), name: 'Anton Mayer', email: 'antom@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-0.svg", initials: "AM"},
+    { id: generateId(), avatarid: rollDice(), name: 'Anja Schulz', email: 'schulz@hotmail.com', phone: '+49123456789', imagePath:"/img/Ellipse5-3.svg", initials: "AS"},
+    { id: generateId(), avatarid: rollDice(), name: 'Benedikt Ziegler', email: 'benedikt@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-4.svg", initials: "BZ"},
+    { id: generateId(), avatarid: rollDice(), name: 'David Eisenberg', email: 'davidberg@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-1.svg", initials: "DE",},
+    { id: generateId(), avatarid: rollDice(), name: 'Eva Fischer', email: 'eva@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-2.svg", initials: "EF"},
+    { id: generateId(), avatarid: rollDice(), name: 'Emmanuel Mauer', email: 'emmanuelma@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-1.svg", initials: "EM"},
+    { id: generateId(), avatarid: rollDice(), name: 'Marcel Bauer', email: 'bauer@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-3.svg", initials: "MB"},
+    { id: generateId(), avatarid: rollDice(), name: 'Tatjana Wolf', email: 'wolf@gmail.com', phone: '+49123456789', imagePath: "/img/Ellipse5-2.svg", initials: "TW"},
 ];
 
 /** generate the demo contacts for a user when you sign up
@@ -31,9 +31,9 @@ function generateDemoTasks() {
                     { description: 'Start Page Layout', checked: false }
                 ],
                 selectedContacts: [
-                    { id: demoContacts[0].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-0.svg", initials: "AM", name: "Anton Mayer"  },
-                    { id: demoContacts[5].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-1.svg", initials: "EM", name: "Emmanuel Mauer"},
-                    { id: demoContacts[6].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-3.svg", initials: "MB", name: "Marcel Bauer" }
+                    { id: demoContacts[0].id, imagePath: "/img/Ellipse5-0.svg", initials: "AM", name: "Anton Mayer"  },
+                    { id: demoContacts[5].id, imagePath: "/img/Ellipse5-1.svg", initials: "EM", name: "Emmanuel Mauer"},
+                    { id: demoContacts[6].id, imagePath: "/img/Ellipse5-3.svg", initials: "MB", name: "Marcel Bauer" }
                 ],
                 priority: 'medium',
                 boardColumn: 'progress-column',
@@ -49,9 +49,9 @@ function generateDemoTasks() {
                 subtasks: 0,
                 subtasksData: [],
                 selectedContacts: [
-                    { id: demoContacts[3].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-1.svg", initials: "DE", name: "David Eisenberg" },
-                    { id: demoContacts[2].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-4.svg", initials: "BZ", name: "Benedikt Ziegler" },
-                    { id: demoContacts[1].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-3.svg", initials: "AS", name: "Anja Schulz" }
+                    { id: demoContacts[3].id, imagePath: "/img/Ellipse5-1.svg", initials: "DE", name: "David Eisenberg" },
+                    { id: demoContacts[2].id, imagePath: "/img/Ellipse5-4.svg", initials: "BZ", name: "Benedikt Ziegler" },
+                    { id: demoContacts[1].id, imagePath: "/img/Ellipse5-3.svg", initials: "AS", name: "Anja Schulz" }
                 ],
                 priority: 'low',
                 boardColumn: 'await-column',
@@ -67,9 +67,9 @@ function generateDemoTasks() {
                 subtasks: 0,
                 subtasksData: [],
                 selectedContacts: [
-                    { id: demoContacts[4].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-2.svg", initials: "EF", name: "Eva Fischer" },
-                    { id: demoContacts[1].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-3.svg", initials: "AS", name: "Anja Schulz" },
-                    { id: demoContacts[7].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-2.svg", initials: "TW", name: "Tatjana Wolf" }
+                    { id: demoContacts[4].id, imagePath: "/img/Ellipse5-2.svg", initials: "EF", name: "Eva Fischer" },
+                    { id: demoContacts[1].id, imagePath: "/img/Ellipse5-3.svg", initials: "AS", name: "Anja Schulz" },
+                    { id: demoContacts[7].id, imagePath: "/img/Ellipse5-2.svg", initials: "TW", name: "Tatjana Wolf" }
                 ],
                 priority: 'medium',
                 boardColumn: 'await-column',
@@ -88,8 +88,8 @@ function generateDemoTasks() {
                     { description: 'Setup Base Styles', checked: true }
                 ],
                 selectedContacts: [
-                    { id: demoContacts[0].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-0.svg", initials: "AM", name: "Anton Mayer" },
-                    { id: demoContacts[2].id, imagePath: "http://127.0.0.1:5501/img/Ellipse5-4.svg", initials: "BZ", name: "Benedikt Ziegler" }
+                    { id: demoContacts[0].id, imagePath: "/img/Ellipse5-0.svg", initials: "AM", name: "Anton Mayer" },
+                    { id: demoContacts[2].id, imagePath: "/img/Ellipse5-4.svg", initials: "BZ", name: "Benedikt Ziegler" }
                 ],
                 priority: 'urgent',
                 boardColumn: 'done-column',
